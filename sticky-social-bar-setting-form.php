56c31d4b1940f8eb8479257981403d3517340cbb
Eftakhairul Islam <eftakhairul@gmail.com>
2013-07-15 03:04:43 +0600

    Form is updated

--- sticky-social-bar-setting-form.php
+++ sticky-social-bar-setting-form.php
@@ -37,48 +37,79 @@ $link   = (!empty($_POST['link'])) ? $_POST['link'] : get_object_vars($result[0]
 
     <form method="post" action="<?php echo $_SERVER['REQUEST_URI']?>">
         <?php wp_nonce_field('update-options'); ?>
-        <label for="link[facebook]">
-            Facebook:
-        </label>
-        <input type="text" name="link[facebook]" id='facebook' value="<?php echo $link['facebook']; ?>"/>
-        <br/>
+        <table>
+            <tr>
+                <td>
+                    <label for="link[facebook]">
+                        Facebook:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[facebook]" id='facebook' value="<?php echo $link['facebook']; ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="link[facebook]">
+                        Twitter:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[twitter]" id='twitter' value="<?php echo $link['twitter']; ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="link[facebook]">
+                        Vemo:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[vemo]" id='vemo' value="<?php echo $link['vemo']; ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="link[facebook]">
+                        Pinterest:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[pinterest]" id='pinterest' value="<?php echo $link['pinterest']; ?>"/>
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <label for="link[facebook]">
+                        RSS:
+                    </label>
 
-        <label for="link[facebook]">
-            Twitter:
-        </label>
-        <input type="text" name="link[twitter]" id='twitter' value="<?php echo $link['twitter']; ?>"/>
-        <br/>
-
-        <label for="link[facebook]">
-            Vemo:
-        </label>
-        <input type="text" name="link[vemo]" id='vemo' value="<?php echo $link['vemo']; ?>"/>
-        <br/>
-
-
-        <label for="link[facebook]">
-            Pinterest:
-        </label>
-        <input type="text" name="link[pinterest]" id='pinterest' value="<?php echo $link['pinterest']; ?>"/>
-        <br/>
-
-        <label for="link[facebook]">
-            RSS:
-        </label>
-        <input type="text" name="link[rss]" id='rss' value="<?php echo $link['rss']; ?>"/>
-        <br/>
-
-        <label for="link[linkedin]">
-            LinkedIn:
-        </label>
-        <input type="text" name="link[linkedin]" id='linkedin' value="<?php echo $link['linkedin']; ?>"/>
-        <br/>
-
-        <label for="link[linkedin]">
-            Tumblr:
-        </label>
-        <input type="text" name="link[tumblr]" id='tumblr' value="<?php echo $link['tumblr']; ?>"/>
-        <br/>
+                </td>
+                <td>
+                    <input type="text" name="link[rss]" id='rss' value="<?php echo $link['rss']; ?>"/>
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <label for="link[linkedin]">
+                        LinkedIn:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[linkedin]" id='linkedin' value="<?php echo $link['linkedin']; ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="link[linkedin]">
+                        Tumblr:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" name="link[tumblr]" id='tumblr' value="<?php echo $link['tumblr']; ?>"/>
+                </td>
+            </tr>
+        </table>
 
         <input type="submit" class="button-primary" name="save-fav-icon" value="<?php _e('Save Changes') ?>"/>
         <input type="hidden" name="action" value="update"/>
